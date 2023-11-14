10ff3985564ed6e856fee87cf148389146a00398
benidate <zeroquakeapp@gmail.com>
2023-11-14 14:28:19 +0900

    可読性向上

--- src/main.js
+++ src/main.js
@@ -3064,39 +3064,25 @@ function shindoConvert(str, responseType) {
         ShindoTmp = 12;
     }
   } else {
-    if (str < 0.5) {
-      ShindoTmp = 0;
-    } else if (str < 1.5) {
-      ShindoTmp = 1;
-    } else if (str < 2.5) {
-      ShindoTmp = 2;
-    } else if (str < 3.5) {
-      ShindoTmp = 3;
-    } else if (str < 4.5) {
-      ShindoTmp = 4;
-    } else if (str < 5) {
-      ShindoTmp = 5;
-    } else if (str < 5.5) {
-      ShindoTmp = 6;
-    } else if (str < 6) {
-      ShindoTmp = 7;
-    } else if (str < 6.5) {
-      ShindoTmp = 8;
-    } else if (6.5 <= str) {
-      ShindoTmp = 9;
-    } else if (7.5 <= str) {
-      ShindoTmp = 10;
-    } else {
-      ShindoTmp = 12;
-    }
+    if (str < 0.5) ShindoTmp = 0;
+    else if (str < 1.5) ShindoTmp = 1;
+    else if (str < 2.5) ShindoTmp = 2;
+    else if (str < 3.5) ShindoTmp = 3;
+    else if (str < 4.5) ShindoTmp = 4;
+    else if (str < 5) ShindoTmp = 5;
+    else if (str < 5.5) ShindoTmp = 6;
+    else if (str < 6) ShindoTmp = 7;
+    else if (str < 6.5) ShindoTmp = 8;
+    else if (6.5 <= str) ShindoTmp = 9;
+    else if (7.5 <= str) ShindoTmp = 10;
+    else ShindoTmp = 12;
   }
-  var ConvTable;
   switch (responseType) {
     case 1:
-      ConvTable = ["0", "1", "2", "3", "4", "5弱", "5強", "6弱", "6強", "7", "7以上", "５弱以上未入電", "不明"];
+      var ConvTable = ["0", "1", "2", "3", "4", "5弱", "5強", "6弱", "6強", "7", "7以上", "５弱以上未入電", "不明"];
       break;
     case 2:
-      ConvTable = [
+      var ConvTable = [
         [config.color.Shindo["0"].background, config.color.Shindo["0"].color],
         [config.color.Shindo["1"].background, config.color.Shindo["1"].color],
         [config.color.Shindo["2"].background, config.color.Shindo["2"].color],
@@ -3113,17 +3099,17 @@ function shindoConvert(str, responseType) {
       ];
       break;
     case 3:
-      ConvTable = [null, "1", "2", "3", "4", "5m", "5p", "6m", "6p", "7", "7p", "5p?", null];
+      var ConvTable = [null, "1", "2", "3", "4", "5m", "5p", "6m", "6p", "7", "7p", "5p?", null];
       break;
     case 4:
-      ConvTable = [0, 1, 2, 3, 4, 4.5, 5, 5.5, 6, 7, 7.5, 4.5, null];
+      var ConvTable = [0, 1, 2, 3, 4, 4.5, 5, 5.5, 6, 7, 7.5, 4.5, null];
       break;
     case 5:
-      ConvTable = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, null, 11];
+      var ConvTable = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, null, 11];
       break;
     case 0:
     default:
-      ConvTable = ["0", "1", "2", "3", "4", "5-", "5+", "6-", "6+", "7", "7+", "未", "?"];
+      var ConvTable = ["0", "1", "2", "3", "4", "5-", "5+", "6-", "6+", "7", "7+", "未", "?"];
       break;
   }
   return ConvTable[ShindoTmp];
