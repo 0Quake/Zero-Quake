ba42048d9411f76746bad4af4324c20d6525391e
Benidate <sanpuinwa@gmail.com>
2023-12-19 02:37:13 +0900

    観測点データの修正

--- tools/tools.js
+++ tools/tools.js
@@ -7,6 +7,52 @@ for (const elm of data) {
   }
 }
 
+//画像上で重なっているであろう観測点の組を表示
+var group = {};
+a.forEach(function (elm) {
+  b = a.filter(function (elm2) {
+    return Math.abs(elm2.Point.X - elm.Point.X) <= 1 && Math.abs(elm2.Point.Y - elm.Point.Y) <= 1 && elm.Code !== elm2.Code && !elm.IsSuspended && !elm.IsSuspended;
+  });
+  b.forEach(function (elm2) {
+    if (group[elm.Code]) {
+      if (!group[elm.Code].includes(elm2.Code)) group[elm.Code].push(elm2.Code);
+    } else if (group[elm2.Code]) {
+      if (!group[elm2.Code].includes(elm.Code)) group[elm2.Code].push(elm.Code);
+    } else group[elm.Code] = [elm2.Code];
+  });
+});
+var c = "";
+Object.keys(group).forEach(function (elm) {
+  c += elm + "と" + group[elm].join(",") + "\n";
+});
+console.log(c);
+
+//maplibreで上の項目の組を順に表示
+a.replaceAll("と", ",").split("|");
+var i = 0;
+function aa() {
+  document.querySelectorAll(".marker-circle").forEach(function (elm) {
+    elm.style.border = "none";
+  });
+  var q = 0;
+  b[Math.min(i, b.length)].split(",").forEach(function (elm2) {
+    var pt = document.querySelector(".KmoniPoint_" + elm2);
+    if (pt) {
+      pt.style.border = "solid 10px #FFF";
+      q++;
+    }
+  });
+  console.log(i + "/" + b.length, q + "件");
+}
+document.onkeydown = function (e) {
+  if (e.keyCode == 37) {
+    i--;
+  } else if (e.keyCode == 37) {
+    i++;
+  }
+  aa();
+};
+
 function geosailing(latA, lngA, latB, lngB) {
-    return Math.acos(Math.sin(Math.atan(Math.tan(latA * (Math.PI / 180)))) * Math.sin(Math.atan(Math.tan(latB * (Math.PI / 180)))) + Math.cos(Math.atan(Math.tan(latA * (Math.PI / 180)))) * Math.cos(Math.atan(Math.tan(latB * (Math.PI / 180)))) * Math.cos(lngA * (Math.PI / 180) - lngB * (Math.PI / 180))) * 6371.008;
+  return Math.acos(Math.sin(Math.atan(Math.tan(latA * (Math.PI / 180)))) * Math.sin(Math.atan(Math.tan(latB * (Math.PI / 180)))) + Math.cos(Math.atan(Math.tan(latA * (Math.PI / 180)))) * Math.cos(Math.atan(Math.tan(latB * (Math.PI / 180)))) * Math.cos(lngA * (Math.PI / 180) - lngB * (Math.PI / 180))) * 6371.008;
 }
