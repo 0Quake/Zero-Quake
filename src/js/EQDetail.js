45a6bfbba6a3bd4d0da941ea3b0b95805a144b21
Benidate <sanpuinwa@gmail.com>
2024-10-06 19:25:11 +0900

    海底地形の描画変更に伴って地図のデザイン改善

--- src/js/EQDetail.js
+++ src/js/EQDetail.js
@@ -377,7 +377,7 @@ function Mapinit() {
           type: "line",
           source: "worldmap",
           paint: {
-            "line-color": "#444",
+            "line-color": "#666",
             "line-width": 1,
           },
         },
