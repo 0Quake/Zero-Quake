bfc5f7da0dbf8108f2eb30694aeed3bdd3d166c1
Benidate <93989835+0Quake@users.noreply.github.com>
2025-03-08 20:23:46 +0900

    気象庁防災情報XMLより一部電文を取得できないことがある問題

--- src/main.js
+++ src/main.js
@@ -3153,6 +3153,7 @@ function Req_JMAXMLList(LongPeriodFeed, count) {
                 title == "震源に関する情報" ||
                 title == "震源・震度に関する情報" ||
                 title == "長周期地震動に関する観測情報" ||
+                title == "遠地地震に関する情報" ||
                 title == "顕著な地震の震源要素更新のお知らせ"
               ) {
                 if (EQInfoCount <= config.Info.EQInfo.ItemCount)
@@ -3160,7 +3161,9 @@ function Req_JMAXMLList(LongPeriodFeed, count) {
                 if (title == "震源・震度に関する情報") EQInfoCount++; //「震源・震度に関する情報」の件数≒地震の数 のためカウント
               } else if (
                 title == "津波情報a" ||
-                title == "津波警報・注意報・予報a"
+                title == "津波警報・注意報・予報a" ||
+                title == "沖合の津波観測に関する情報" ||
+                title == "北海道・三陸沖後発地震注意情報"
               )
                 Req_JMAXML(url, count);
             }
